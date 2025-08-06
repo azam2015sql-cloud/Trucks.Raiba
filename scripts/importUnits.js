9387c14ed9db8a5639b02cac5e3a120d5966d2e6
azam2015sql-cloud <azam2015sql@gmail.com>
2025-08-06 23:20:28 +0200

    Update importUnits.js

--- scripts/importUnits.js
+++ scripts/importUnits.js
@@ -1,1 +1,22 @@
 
+const mongoose = require('mongoose');
+const xlsx = require('xlsx');
+const Unit = require('../models/unit.model');
+const config = require('../config/config');
+
+mongoose.connect(config.database.uri, config.database.options)
+  .then(() => {
+    const workbook = xlsx.readFile(process.argv[2]);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const units = xlsx.utils.sheet_to_json(sheet);
+    
+    Unit.insertMany(units)
+      .then(() => {
+        console.log(`تم استيراد ${units.length} وحدة بنجاح`);
+        process.exit(0);
+      })
+      .catch(err => {
+        console.error('حدث خطأ أثناء الاستيراد:', err);
+        process.exit(1);
+      });
+  });
