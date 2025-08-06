85bf26c646096047e7af4413304426fe5679ec0d
azam2015sql-cloud <azam2015sql@gmail.com>
2025-08-06 23:17:56 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const unitsController = require('../controllers/units.controller');
+
+router.get('/units', unitsController.getAllUnits);
+router.post('/units/move', unitsController.moveUnit);
+
+module.exports = router;
